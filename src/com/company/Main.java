1da9b956076c2be0230d7ae072089444f961efdc
Olsson2000 <jollemannen00@gmail.com>
2019-12-09 09:36:28 +0100

    .idea/workspace.xml

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public int add(int first, int second) {
+	// write your code here
+        return first;
+    }
+
+        public int sub(int first, int second)
+
+}
+}
