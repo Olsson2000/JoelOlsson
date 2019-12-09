b9ae7f062065b4c0b9cc285da3304472025c524b
Olsson2000 <jollemannen00@gmail.com>
2019-12-09 09:45:10 +0100

    src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,9 @@
 package com.company;
 
-public class Main {
 
-    public int add(int first, int second) {
+   public class Math {
+
+        public int add(int first, int second) {
 	// write your code here
         return first;
     }
